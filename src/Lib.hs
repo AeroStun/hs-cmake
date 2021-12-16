3d7f7387ec3a756a8b7c1fafb16414b2af6ceeee
AeroStun <24841307+AeroStun@users.noreply.github.com>
2021-12-16 01:26:12 +0100

    [Lib] Bind variables reflecting the CWD

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,11 +3,11 @@ module Lib (
   run_,
   runFile
   ) where
-import           CMake.AST.Defs          (File)
 import qualified CMake.AST.Parser        as AST
 import           CMake.Interpreter       (cmPrelude, processFile)
-import           CMake.Interpreter.State (CmState)
+import           CMake.Interpreter.State (CmState (..), setVariable)
 import           Data.Functor            (void, ($>))
+import           System.Directory        (getCurrentDirectory)
 import           Text.Trifecta           (parseFromFile, parseString)
 import           Text.Trifecta.Result    (ErrInfo (..), Result (..))
 
@@ -24,6 +24,12 @@ run_ s = case parseString AST.file mempty s of
 runFile :: String -> IO ()
 runFile s = do
    result <- parseFromFile AST.file s
+   cwd <- getCurrentDirectory
    case result of
-     Just program -> void $ processFile program cmPrelude
+     Just program -> void $ processFile program $ setVars cwdVars cwd cmPrelude
      Nothing      -> pure ()
+  where
+    setVars :: [String] -> String -> CmState -> CmState
+    setVars vs val st = foldl (\p@CmState{currentScope=ps} v -> p{currentScope=setVariable v val ps}) st vs
+    cwdVars :: [String]
+    cwdVars = ["CMAKE_SOURCE_DIR", "CMAKE_BINARY_DIR", "CMAKE_CURRENT_SOURCE_DIR", "CMAKE_CURRENT_BINARY_DIR"]
